58534e1f28d8d0c15edf4642d98c99b3cc674727
gookudog321 <gookudog321@hotmail.com>
2020-10-08 08:47:07 +0700

    Updates

---
+++ precache-manifest.6c9f0b041a18589acede161a5382bceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd99b5be201af6a7d048c982522100f",
+    "url": "/lanahavest/index.html"
+  },
+  {
+    "revision": "a3e626e1550d6fa5b596",
+    "url": "/lanahavest/static/css/main.ffc25106.chunk.css"
+  },
+  {
+    "revision": "db05fbe51e0321f95fe5",
+    "url": "/lanahavest/static/js/2.6d2eb9d4.chunk.js"
+  },
+  {
+    "revision": "c741a9512219825c71dd1ad1178add62",
+    "url": "/lanahavest/static/js/2.6d2eb9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e626e1550d6fa5b596",
+    "url": "/lanahavest/static/js/main.7b58fb8c.chunk.js"
+  },
+  {
+    "revision": "2ba909f8bf36a009e38c",
+    "url": "/lanahavest/static/js/runtime-main.97549b0b.js"
+  },
+  {
+    "revision": "4dc9f612290474b6b33dea0d787401ff",
+    "url": "/lanahavest/static/media/lannaharvest.4dc9f612.png"
+  }
+]);
