7ab7319ee23c0c83f0f2a22fa55a599c74542d17
gookudog321 <gookudog321@hotmail.com>
2020-10-10 12:21:35 +0700

    up date

--- src/component/Product.js
+++ src/component/Product.js
@@ -7,7 +7,8 @@ export default class Product extends Component {
             <section className="product">
                 <h3>Us Product</h3>
                 <p className="p1" />
-                <div className="product-grid container">
+                <div className="container">
+                <div className="product-grid ">
                         {/* image */}
                         <div>
                             <img  src="https://picsum.photos/500/500" />
@@ -32,6 +33,7 @@ export default class Product extends Component {
                         <div>
                             <p>  Speedily say has suitable disposal add boy. On forth doubt miles of child. Exercise joy man children rejoiced. Yet uncommonly his ten who diminution astonished. Demesne new manners savings staying had. Under folly balls death own point now men. Match way these she avoid see death. She whose drift their fat off. </p>
                         </div>
+                </div>
                 </div>
                <Link to="/product"><button className="product-button">Real More</button></Link>
             </section>
